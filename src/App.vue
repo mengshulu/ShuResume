f56a0fadb0783a9eb4a7bffd7ed7a165e8baf894
RShu <breezecoo1123@gmail.com>
2023-05-03 19:44:29 +0800

    Feat: 新增 header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,16 @@
 <template>
   <header>
+    <h1 class="logo">
+      RShuResume
+    </h1>
     <div class="wrapper">
-
       <nav>
-        <RouterLink to="/">Home</RouterLink>
+        <div
+          id="menu"
+          class="menu"
+        >
+          <span class="material-symbols-outlined"> menu</span>
+        </div>
       </nav>
     </div>
   </header>
@@ -11,9 +18,24 @@
   <RouterView />
 </template>
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
+<style lang="scss" scoped>
+header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: space-between;
+  padding: 15px;
+  width: 100%;
+  height: 60px;
+  background: #fff;
+}
 
-<style scoped>
+.logo {
+  font-size: 18px;
+  font-weight: bold;
+}
 </style>
